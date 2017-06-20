746b674370605492dca6f829d98340680367613b
Ian <ian.nesbitt@gmail.com>
2017-06-20 15:46:26 -0400

    Update ffmpeg_video.sh

--- ffmpeg_video.sh
+++ ffmpeg_video.sh
@@ -1,24 +1,25 @@
 #! /bin/bash
 
-# Written by Ian Nesbitt, e4sciences LLC, 2016
+# Written by Ian Nesbitt, 2017
 # Requires ffmpeg (see https://github.com/ccrisan/motioneye/wiki/Install-On-Raspbian)
+# and some crontab work as well
 
 if [ -d "/home/pi/bin/pywx/img/arch/$(date +\%F)" ]; then
-  cd /home/pi/bin/netcamscp/img/arch/$(date +\%F)
+  cd /home/pi/bin/pywx/img/arch/$(date +\%F)
 
   # writes list of files, numbers each list item,
   # stores number and filename as vars, then renames files with numbering.
   # numbering is in %d format starting at 1.jpg
   ls | cat -n | while read n f; do mv "$f" "$n.jpg"; done
   # converts filenames with %d format to 30fps mp4 starting at 1.jpg
-  ffmpeg -start_number 1 -r 30  -i \%d.jpg -c:v libx264 $(date +\%F).mp4
+  ffmpeg -start_number 1 -r 30  -i \%d.jpg -c:v libx264 ../yesterday.mp4
 
   # removes jpgs
   rm *.jpg
-  # moves .mp4 up 1 level
-  mv $(date +\%F).mp4 ../
-  # removes current datestamped folder
-  #cd ../; rm -r $(date +\%F)/
+  # moves .mp4 somewhere useful
+  #mv yesterday.mp4 ./ ## <<change this location
+  # removes current datestamped folder (assumes cron will create a new one)
+  cd ../; rm -r $(date +\%F)/
 
   cd ~/
 fi
